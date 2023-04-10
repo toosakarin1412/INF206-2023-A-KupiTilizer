4c318a38fc4333110cd97b7111b404f8e33320b7
toosakarin1412 <toosakarin1412@gmail.com>
2023-04-10 08:40:25 +0700

    feat: halaman setting, market, coupon

---
+++ Kupitilizer/app/Http/Controllers/UserController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\ProfileUpdateRequest;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\View\View;
+
+class UserController extends Controller
+{
+    public function userHome()
+    {
+        return view('home');
+    }
+}
