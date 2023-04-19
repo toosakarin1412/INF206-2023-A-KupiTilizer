57755a49321a8d350257b7ba43112aab6f0349eb
Fatiya Humaira Yunaz <fatiya33@mhs.unsyiah.ac.id>
2023-04-19 22:59:51 +0700

    fix: link manageuser

--- Kupitilizer/resources/views/layouts/dashboardLayout.blade.php
+++ Kupitilizer/resources/views/layouts/dashboardLayout.blade.php
@@ -67,7 +67,7 @@
                 </li>
                 @endif
                 <li>
-                    <a href="/<?php echo Auth::user()->role?>/manageadmin" class="flex items-center p-2 transition duration-75 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700 dark:text-white group">
+                    <a href="/<?php echo Auth::user()->role?>/manageuser" class="flex items-center p-2 transition duration-75 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700 dark:text-white group">
                     <svg aria-hidden="true" class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" fill="white" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd"></path></svg>
                     <span class="ml-3">Manage Users</span>
                     </a>
