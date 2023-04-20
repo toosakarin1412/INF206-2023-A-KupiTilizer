6d58f4e8a96348e0a2aa4a42baa1710952ad1a16
furqanramadhan <104356657+furqanramadhan@users.noreply.github.com>
2023-04-21 01:06:22 +0700

    fix: border

---
+++ Kupitilizer/resources/views/aboutus.blade.php
@@ -0,0 +1,81 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="w-full items-center justify-center">
+    <div>
+    <h1 class="text-center">Our Team</h1>    
+    </div>
+    <div>
+        <div class="flex md:grid-cols-3 grid-cols-1 py-20 px-20 items-center mx-2 border-2 justify-between">   
+            <div class="bg-blue-100">
+                <div>
+                    <!-- Padding here -->
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-60 h-60">
+                    <!-- Border radius 100 -->
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
+                    </svg>
+                </div> 
+                <div>
+                    <h1 class ="text-center">Diky Wahyudi</h1>
+                </div>
+                <div>3</div>
+                <div>4</div>
+            </div>
+            <div class="bg-red-100">
+                <div>
+                    <!-- Padding here -->
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-60 h-60">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
+                    </svg>
+                </div> 
+                <div>
+                <h1 class ="text-center">Rahmat Azrima</h1>
+                </div>
+                <div>3</div>
+                <div>4</div>
+            </div>
+            <div class="bg-gray-100">
+                <div>
+                    <!-- Padding here -->
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-60 h-60">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
+                    </svg>
+                </div> 
+                <div>
+                <h1 class ="text-center">Furqan Ramadhan</h1>
+                </div>
+                <div>3</div>
+                <div>4</div>
+            </div>
+        </div>
+        <div class="flex md:grid-cols-2 grid-cols-1 py-20 px-20 items-center mx-2 border-2 justify-center">
+            <div>
+                <div>
+                    <!-- Padding here -->
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-60 h-60">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
+                    </svg>
+                </div> 
+                <div>
+                <h1 class ="text-center">M. Danish Rabbani</h1>
+                </div>
+                <div>3</div>
+                <div>4</div>
+            </div>
+            <div>
+                <div>
+                    <!-- Padding here -->
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-60 h-60">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
+                    </svg>
+                </div> 
+                <div>
+                <h1 class ="text-center">Fatiya Humaira Yunaz</h1>
+                </div>
+                <div>3</div>
+                <div>4</div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
--- Kupitilizer/routes/web.php
+++ Kupitilizer/routes/web.php
@@ -25,6 +25,10 @@ Route::get('/', function () {
     return view('home');
 })->name('home');
 
+Route::get('/aboutus', function(){
+    return view('aboutus');
+})->name('aboutus');
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
