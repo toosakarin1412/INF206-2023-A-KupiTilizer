b49ae07cf55263c3e1cb933953d70074c9139c1f
Fatiya Humaira Yunaz <fatiya33@mhs.unsyiah.ac.id>
2023-04-23 11:19:04 +0700

    fix: tampilan tabel

--- Kupitilizer/resources/views/manageuser.blade.php
+++ Kupitilizer/resources/views/manageuser.blade.php
@@ -37,9 +37,6 @@
         <table class="w-full text-sm text-left text-gray-500 ">
             <thead class="text-xs text-gray-700 bg-gray-50">
                 <tr>
-                    <th scope="col" class="px-6 py-3">
-                        No
-                    </th>
                     <th scope="col" class="px-6 py-3 w-1/4">
                         Nama
                     </th>
@@ -52,42 +49,17 @@
                 </tr>
             </thead>
             <tbody>
-            <tr class="bg-white hover:bg-gray-50 rounded-xl">
-                    <th scope="row" class="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap dark:text-white">
-                        1
-                    </th>
-                    <td class="px-6 py-4">
-                        Abcd
-                    </td>
-                    <td class="px-6 py-4">
-                        Abcd@gmail.com
-                    </td>
+                @foreach($users as $item)
+                <tr class="bg-white hover:bg-gray-50 rounded-xl">
+                    <th scope="col" class="px-6 py-3 w-1/4">{{ $item-> name }} </th>
+                    <th scope="col" class="px-6 py-3">{{ $item-> email}} </th>
                     <td class="px-6 py-4 flex gap-1">
                         <!-- Modal toggle -->
-                        <a href="#" type="button" data-modal-target="editUserModal" data-modal-show="editUserModal" class="bg-blue-300 text-white font-bold rounded-md px-4 py-2">Detail</a>
                         <a href="#" type="button" class="bg-leaf text-white font-bold rounded-md px-4 py-2">Edit</a>
                         <a href="#" type="button" class="bg-red-400 text-white font-bold rounded-md px-4 py-2">Delete</a>                    
                     </td>
                 </tr>
-                
-                <tr class="bg-white hover:bg-gray-50 rounded-xl">
-                    <th scope="row" class="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap dark:text-white">
-                        2
-                    </th>
-                    <td class="px-6 py-4">
-                        Yahdina Ahsya
-                    </td>
-                    <td class="px-6 py-4">
-                        Yahdina@gmail.com
-                    </td>
-                    <td class="px-6 py-4 flex gap-1">
-                        <!-- Modal toggle -->
-                        <a href="#" type="button" data-modal-target="editUserModal" data-modal-show="editUserModal" class="bg-blue-300 text-white font-bold rounded-md px-4 py-2">Detail</a>
-                        <a href="#" type="button" data-modal-target="editUserModal" data-modal-show="editUserModal" class="bg-leaf text-white font-bold rounded-md px-4 py-2">Edit</a>
-                        <a href="#" type="button" data-modal-target="editUserModal" data-modal-show="editUserModal" class="bg-red-400 text-white font-bold rounded-md px-4 py-2">Delete</a>                    
-                    </td>
-                </tr>
-
+                @endforeach
             </tbody>            
         </div>
     </div>    
@@ -99,7 +71,7 @@
     <div class="relative w-full h-full max-w-2xl md:h-auto">
         <!-- Modal content -->
         <div class="relative bg-white rounded-lg shadow">
-            <button type="button" class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white" data-modal-hide="register-modal">
+            <button type="button" class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white" data-modal-hide="tambah-modal">
                 <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                 <span class="sr-only">Close modal</span>
             </button>
@@ -107,7 +79,7 @@
                 <div>
                     <img class = "h-auto mx-auto w-5/12" src="{{asset('images/logo.png')}}" alt="">
                 </div>
-                <h3 class="mb-4 text-xl font-medium text-gray-900 dark:text-white">Daftarkan Akun</h3>
+                <h3 class="mb-4 text-xl font-medium text-gray-900 dark:text-white">Tambahkan Akun</h3>
                 <form class="space-y-6" method="POST" action="{{ route('register') }}">
                     @csrf
                     <div class="sm:grid sm:grid-cols-2 gap-4">
