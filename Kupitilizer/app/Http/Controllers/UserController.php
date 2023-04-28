20976077c4e6a7fc58c42ab10451c077d048f24b
Fatiya Humaira Yunaz <fatiya33@mhs.unsyiah.ac.id>
2023-04-28 09:24:06 +0700

    fix: add function add

--- Kupitilizer/app/Http/Controllers/UserController.php
+++ Kupitilizer/app/Http/Controllers/UserController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ProfileUpdateRequest;
+use App\Models\User;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Redirect;
+use Illuminate\Validation\Rules;
 use Illuminate\View\View;
 
 class UserController extends Controller
@@ -25,6 +28,24 @@ class UserController extends Controller
     {
         $users=DB::table('users')->where('role', 'user')->get();
         return view('manageuser',['users'=>$users]);
+    }
+    
+    public function add(Request $request): RedirectResponse
+    {
+        $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:'.User::class],
+            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'role' => 'user',
+            'password' => Hash::make($request->password),
+        ]);
+
+        return redirect('manager/manageuser');
     }
 
 }
