bb91725052093ee6834022da78b2503eb72424f7
Fatiya Humaira Yunaz <fatiya33@mhs.unsyiah.ac.id>
2023-04-29 20:15:04 +0700

    fix: login form view and add comment

--- Kupitilizer/resources/views/layouts/app.blade.php
+++ Kupitilizer/resources/views/layouts/app.blade.php
@@ -189,30 +189,48 @@
                         <span class="sr-only">Close modal</span>
                     </button>
                     <div class="px-6 py-6 lg:px-8">
+
+                        <!-- Logo -->
                         <div>
                             <img class = "h-auto mx-auto w-5/12" src="{{asset('images/logo.png')}}" alt="">
                         </div>
+
+                        <!-- Label form -->
                         <h3 class="mb-4 text-xl font-medium text-gray-900 dark:text-white">Masuk Akun</h3>
                         <form class=" rounded-lg space-y-6 " method="POST" action="{{ route('login') }}">
                             @csrf 
                             <div>
+
+                                <!-- Label input-->
                                 <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Alamat E-mail</label>
+                                
+                                <!-- Input Email-->
                                 <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="name@company.com" required>
                             </div>
                             <div>
+
+                                <!-- Label input-->
                                 <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Kata Sandi</label>
+                                
+                                <!-- Input Password-->
                                 <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required>
                             </div>
+
                             <div class="flex justify-between">
                                 <div class="flex items-start">
                                     <div class="flex items-center h-5">
                                         <input id="remember" type="checkbox" value="" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-600 dark:border-gray-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800">
                                     </div>
+
+                                    <!-- Label ingat saya-->
                                     <label for="remember" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Ingat Saya</label>
+                                
                                 </div>
-                                <a href="{{ route('password.request') }}" class="text-sm text-blue-700 hover:underline dark:text-blue-500">Lupa Kata Sandi?</a>
                             </div>
+                            <!-- Button submit -->
                             <button type="submit" class=" md:mx-auto w-full text-white bg-leaf focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Masuk</button>
+                            
+                            <!-- Button akses register-modal-->
                             <div class="text-sm font-medium text-gray-500 dark:text-gray-300">
                                 <button data-modal-hide="login-modal" data-modal-target="register-modal" data-modal-toggle="register-modal" class="block  text-center sm:py-2 bg-white hover:bg-white hover:text-dark text-leaf font-bold rounded-xl" type="button">Daftar di sini</button>
                             </div>
@@ -227,39 +245,70 @@
             <div class="relative w-full h-full max-w-2xl md:h-auto">
                 <!-- Modal content -->
                 <div class="relative bg-white rounded-lg shadow">
+
+                    <!-- Button close modal-->
                     <button type="button" class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white" data-modal-hide="register-modal">
                         <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                         <span class="sr-only">Close modal</span>
                     </button>
+
                     <div class="px-6 py-6 lg:px-8">
+
+                        <!-- Logo -->
                         <div>
                             <img class = "h-auto mx-auto w-5/12" src="{{asset('images/logo.png')}}" alt="">
                         </div>
+
+                        <!-- Label form -->
                         <h3 class="mb-4 text-xl font-medium text-gray-900 dark:text-white">Daftarkan Akun</h3>
                         <form class="space-y-6" method="POST" action="{{ route('register') }}">
                             @csrf
                             <div class="sm:grid sm:grid-cols-2 gap-4">
                                 <div class="sm:mr-5">
+
+                                    <!-- Label input-->
                                     <label for="name" class="block mb-2 text-sm font-medium text-gray-900">Nama</label>
+
+                                    <!-- Input Nama -->
                                     <input type="name" name="name" id="name" class="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Nama Lengkap Anda" required>
                                 </div>
+
                                 <div>
+
+                                    <!-- Label input-->
                                     <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
+                                    
+                                    <!-- Input Email -->
                                     <input type="email" name="email" id="email" class="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="name@company.com" required>
                                 </div>
+
                                 <div class="sm:mr-5">
+
+                                    <!-- Label input-->
                                     <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Kata Sandi</label>
+                                    
+                                    <!-- Input Password -->
                                     <input type="password" name="password" id="password" placeholder="••••••••" class="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required>
                                 </div>
+
                                 <div>
+
+                                    <!-- Label input-->
                                     <label for="password_confirmation" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Konfirmasi Kata Sandi</label>
+                                    
+                                    <!-- Input Password confirmation -->
                                     <input type="password" name="password_confirmation" id="password" placeholder="••••••••" class="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required autocomplete = "new-password">
                                 </div>
                             </div>
+
+                            <!-- Button submit -->
                             <button type="submit" class="mb-px w-full text-white bg-leaf focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Daftarkan Akun</button>
+                            
+                            <!-- Akses login-modal -->
                             <div class="text-sm font-medium text-gray-500 dark:text-gray-300">
                             Sudah Punya Akun?<button data-modal-hide="register-modal" data-modal-target="login-modal" data-modal-toggle="login-modal" class="block  text-center sm:py-2 bg-white hover:bg-white hover:text-dark text-leaf font-bold rounded-xl" type="button">Login di sini</button>
                             </div>
+
                         </form>
                     </div>
                 </div>
