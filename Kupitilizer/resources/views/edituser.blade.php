bba780cd4e8202b18d79c139ec4308e0f294af9a
Fatiya Humaira Yunaz <fatiya33@mhs.unsyiah.ac.id>
2023-04-29 15:18:33 +0700

    fix: add background

--- Kupitilizer/resources/views/edituser.blade.php
+++ Kupitilizer/resources/views/edituser.blade.php
@@ -23,54 +23,53 @@
 
 
 @section('content')
-@foreach ($user as $item)
-<form method="post" action="/manager/manageuser/update/{{$item->email}}" class="mb-5">
-    @method('patch')
-    @csrf
-    <div class="px-6 py-4 w-full md-:w-1/2 ">
-        <!-- Name -->
-        <div class = "mt-4">
-            <x-input-label for="name" :value="__('Nama')" />
-            <x-text-input id="name" class="w-full block mt-1" type="text" name="name" required value="{{ old('name', $item->name) }}"/>
-            <x-input-error :messages="$errors->get('name')" class="mt-2" />
+<div class="relative overflow-x-auto shadow-md sm:rounded-lg sm:p-4 text-gray-700 border border-gray-200 bg-gray-50">
+    @foreach ($user as $item)
+    <form method="post" action="/manager/manageuser/update/{{$item->email}}" class="mb-5">
+        @method('patch')
+        @csrf
+        <div class="px-6 py-4 w-full md-:w-1/2 ">
+            <!-- Name -->
+            <div class = "mt-4">
+                <x-input-label for="name" :value="__('Nama')" />
+                <x-text-input id="name" class="w-full block mt-1" type="text" name="name" required value="{{ old('name', $item->name) }}"/>
+                <x-input-error :messages="$errors->get('name')" class="mt-2" />
+            </div>
+
+            <!-- Email Address -->
+            <div class="mt-4">
+                <x-input-label for="email" :value="__('Email')" />
+                <x-text-input id="email" class="w-full block mt-1 " type="email" name="email" :value="old('email',$item->email)" required autocomplete="username" />
+                <x-input-error :messages="$errors->get('email')" class="mt-2" />
+            </div>
+
+            <!-- Password -->
+            <div class="mt-4">
+                <x-input-label for="password" :value="__('Password')" />
+
+                <x-text-input id="password" class="block mt-1 w-full"
+                                type="password"
+                                name="password"
+                                required autocomplete="new-password" 
+                                placeholder="••••••••"/>
+
+                <x-input-error :messages="$errors->get('password')" class="mt-2" />
+            </div>
+
+            <!-- Confirm Password -->
+            <div class="mt-4">
+                <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+
+                <x-text-input id="password_confirmation" class="block mt-1 w-full"
+                                type="password"
+                                name="password_confirmation" required autocomplete="new-password" 
+                                placeholder="••••••••"/>
+
+                <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+            </div>
+            <button type="submit" class="mt-4 mb-px w-3/12 text-white bg-leaf focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Update Data Akun</button>
         </div>
-
-        <!-- Email Address -->
-        <div class="mt-4">
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="w-full block mt-1 " type="email" name="email" :value="old('email',$item->email)" required autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
-
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="new-password" 
-                            placeholder="••••••••"/>
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                            type="password"
-                            name="password_confirmation" required autocomplete="new-password" 
-                            placeholder="••••••••"/>
-
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-        </div>
-
-        <button type="submit" class="mt-4 mb-px w-3/12 text-white bg-leaf focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Update Data Akun</button>
-
-    </div>
-</form>
+    </form>
 @endforeach
-
+</div>
 @endsection
