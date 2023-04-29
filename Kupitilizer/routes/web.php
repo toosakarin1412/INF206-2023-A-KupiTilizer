80ede902443f723a82c5a089e3a60f234d7e9ec3
Danish <m_danish@mhs.unsyiah.ac.id>
2023-04-29 14:34:10 +0700

    fix:admincontroller

--- Kupitilizer/routes/web.php
+++ Kupitilizer/routes/web.php
@@ -99,6 +99,7 @@ Route::middleware('auth')->group(function () {
     Route::middleware(['user-access:user'])->group(function () {
         Route::get('/user', [UserController::class, 'userHome']);
 
+
     });
 });
 
