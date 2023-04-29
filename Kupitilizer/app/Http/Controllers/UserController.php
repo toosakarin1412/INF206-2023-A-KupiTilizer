eb0e3b103cdea19546d9a0ae1e6bc6ff0b1fa057
Fatiya Humaira Yunaz <fatiya33@mhs.unsyiah.ac.id>
2023-04-29 19:41:19 +0700

    fix: add javadoc

--- Kupitilizer/app/Http/Controllers/UserController.php
+++ Kupitilizer/app/Http/Controllers/UserController.php
@@ -23,22 +23,36 @@ class UserController extends Controller
 
     /**
      * Mengambil data dari db
+     * @return view('manageuser',['users'=>$users])
      * 
      */
     public function manageUser(): View
-    {
+    {   
+
+        //mengambil data dari database dimana rolenya user
         $users=DB::table('users')->where('role', 'user')->get();
+
+        ///kembali ke laman manage user dengan memberikan data yg telah diambil
         return view('manageuser',['users'=>$users]);
     }
     
+    /**
+     * 
+     * Menambahkan akun user
+     * @param Request $request
+     * @return redirect('manager/manageuser')->with('success', 'User berhasil ditambahkan')
+     * 
+     */
     public function add(Request $request): RedirectResponse
     {
+        //melakukan validasi inputan
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:'.User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
+        //melakukan create user
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
@@ -46,40 +60,77 @@ class UserController extends Controller
             'password' => Hash::make($request->password),
         ]);
 
+        ///kembali ke laman manage user dengan alert succes
         return redirect('manager/manageuser')->with('success', 'User berhasil ditambahkan');
     }
     
+    /**
+     * 
+     * Menghapus akun user
+     * @param $email
+     * @return redirect('manager/manageuser')->with('success', 'User berhasil dihapus')
+     * 
+     */
     public function destroy($email): RedirectResponse
     {
+
+        //menghapus akun dari database dimana email sesuai dengan parameter
         DB::table('users')->where('email', $email)->delete();
+        
+        ///kembali ke laman manage user dengan alert succes
         return redirect('manager/manageuser')->with('success', 'User berhasil dihapus');
     }
 
+    /**
+     * 
+     * Menampilkan kembali data user pada form edit
+     * @param $email
+     * @return return view('edituser',['user'=>$user])
+     * 
+     */
     public function show($email): View
-    {   $user=DB::table('users')->where('email', $email)->get();
+    {   
+
+        //mengambil data dari database dimana emailnya sesuai dengan parameter
+        $user=DB::table('users')->where('email', $email)->get();
+
+        //return ke edituser untuk menampilkan laman edit data
         return view('edituser',['user'=>$user]);
     }
 
+    /**
+     * 
+     * Mengupdate data akun user
+     * @param Request $request, $email
+     * @return redirect('manager/manageuser')->with('success', 'User berhasil ditambahkan');
+     * 
+     */
     public function update(Request $request, $email): RedirectResponse
     {   
 
+        //aturan untuk inputan data
         $rules = [
             'name' => ['required', 'string', 'max:255'],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ];
 
+        //rules untuk email jika diganti
         if($request->email != $email){
             $rules['email'] = ['required', 'string', 'email', 'max:255', 'unique:'.User::class];
         }
         
+        //validasi inputan dengan rules
         $request->validate($rules);
 
+        //melakukan update data
         DB::table('users')->where('email', $email)
             ->update([
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
             ]);        
+        
+        //kembali ke laman manage user dengan alert succes
         return redirect('manager/manageuser')->with('success', 'Data user berhasil diedit!');
     }
 
