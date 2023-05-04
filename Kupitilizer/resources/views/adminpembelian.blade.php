7cd0921395ee93bbb3dec0151411ee8117298ffc
Danish <m_danish@mhs.unsyiah.ac.id>
2023-05-04 07:42:42 +0700

    fix:BE AdminPembelian

--- Kupitilizer/resources/views/adminpembelian.blade.php
+++ Kupitilizer/resources/views/adminpembelian.blade.php
@@ -10,7 +10,7 @@
 <li>
     <div class="flex items-center">
         <svg aria-hidden="true" class="w-6 h-6 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
-        <a href="#" class="ml-1 text-sm font-medium text-gray-700 hover:text-leaf md:ml-2 dark:text-gray-400 dark:hover:text-white">Pembelian</a>
+        <a href="#" class="ml-1 text-sm font-medium text-gray-700 hover:text-leaf md:ml-2 dark:text-gray-400 dark:hover:text-white">Penjemputan</a>
     </div>
 </li>
 @endsection
@@ -31,7 +31,7 @@
             <thead class="text-xs text-gray-700 bg-gray-50">
                 <tr>
                     <th scope="col" class="px-6 py-3">
-                        IDPenjemputan
+                        IDIDPenjemputan
                     </th>
                     <th scope="col" class="px-6 py-3 w-1/4">
                         User
@@ -45,41 +45,41 @@
                 </tr>
             </thead>
             <tbody>
-                @foreach($dataRequest as $item)
+                
                 <tr class="bg-white hover:bg-gray-50 rounded-xl">
                     <th scope="row" class="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap dark:text-white">
-                        {{$item->id}}
+                        123321221321
                     </th>
                     <td class="px-6 py-4">
-                        {{$item->name}}
+                        User
                     </td>
                     <td class="px-6 py-4">
                         <div class="flex items-center">
-                            @if($item->status == "waiting")
+                           
                             <div class="h-2.5 w-2.5 rounded-full bg-yellow-400 mr-2"></div>
-                            @elseif($item->status == "accepted")
+                            
                             <div class="h-2.5 w-2.5 rounded-full bg-green-400 mr-2"></div>
-                            @else
+                            
                             <div class="h-2.5 w-2.5 rounded-full bg-red-400 mr-2"></div>
-                            @endif
-                            <span class="capitalize">{{$item->status}}</span>
+                            
+                            <span class="capitalize"></span>
                         </div>
                     </td>
                     <td class="px-6 py-4 flex gap-1">
-                        <form action="/<?php echo Auth::user()->role?>/requestjemput/detail/{{$item->id}}" method="get" class='d-inline'>
+                        <form action="/<?php echo Auth::user()->role?>" method="get" class='d-inline'>
                             <button type="submit" class="bg-blue-300 text-white font-bold rounded-md px-4 py-2">Detail</button>
                         </form>
-                        @if($item->status == "waiting")
-                        <form action="/<?php echo Auth::user()->role?>/requestjemput/accept/{{$item->id}}" method="get" class='d-inline'>
+                        
+                        <form action="/<?php echo Auth::user()->role?>" method="get" class='d-inline'>
                             <button type="submit" class="bg-leaf text-white font-bold rounded-md px-4 py-2">Accept</button>
                         </form>
-                        <form action="/<?php echo Auth::user()->role?>/requestjemput/decline/{{$item->id}}" method="get" class='d-inline'>
+                        <form action="/<?php echo Auth::user()->role?>" method="get" class='d-inline'>
                             <button type="submit" class="bg-red-400 text-white font-bold rounded-md px-4 py-2">Decline</button>
                         </form>
-                        @endif                  
+                                          
                     </td>
                 </tr>
-                @endforeach
+                
             </tbody>
         </table>
     </div>
