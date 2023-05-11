980c0fb51c84e867ddc6174e34deb5ab881b82cf
Danish <m_danish@mhs.unsyiah.ac.id>
2023-05-11 17:08:46 +0700

    feat:in nohp

--- Kupitilizer/resources/views/usersetting.blade.php
+++ Kupitilizer/resources/views/usersetting.blade.php
@@ -21,7 +21,9 @@
             </div>
             <div class="grid grid-cols-2">
                 <div class="px-4 py-2 font-semibold">Nomor HP</div>
-                <div class="px-4 py-2">-</div>
+                <<div class="px-4 py-2">
+        <input type="text" class="px-4 py-2 border border-gray-300 rounded-md" value="">
+    </div>
             </div>
             <div class="grid grid-cols-2">
                 <div class="px-4 py-2 font-semibold">Alamat</div>
