d9504b49cba64824b8d8564917619ea9c65c25e6
Danish <m_danish@mhs.unsyiah.ac.id>
2023-05-11 18:05:42 +0700

    feat: admin penjualan

--- Kupitilizer/app/Http/Controllers/PembelianController.php
+++ Kupitilizer/app/Http/Controllers/PembelianController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Pembelian;
 use App\Models\Keranjang;
+use App\Models\User;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
 use \Carbon\Carbon;
@@ -12,7 +13,8 @@ use \Carbon\Carbon;
 class PembelianController extends Controller
 {
     public function index(){
-        return view('adminpembelian');
+        $data = DB::table('pembelians')->get();
+        return view('adminpembelian', ['dataRequest' => $data]);
     }
 
     public function beli(Request $request){
@@ -33,5 +35,45 @@ class PembelianController extends Controller
         Keranjang::where('user_id', Auth::user()->id)->delete();
 
         return redirect()->back()->with('success', 'Barang berhasil di pesan');
+    }
+
+    public function detail($id)
+    {
+        $data = DB::table('pembelians')->where('id', $id)->get();
+
+        return view('detailAdminPembelian', ['data' => $data[0]]);
+    }
+
+    public function acceptRequest($id)
+    {
+        $kurir = User::where('role', 'kurir')->get('id');
+        $arrKurir = [];
+        foreach ($kurir as $item) {
+           array_push($arrKurir, $item->id);
+        }
+        $random_keys = array_rand($arrKurir,1);
+        // dd($random_keys);
+        DB::table('pembelians')->where('id', $id)->update([
+            'status' => 'process',
+            'kurir_id' => $arrKurir[$random_keys],
+        ]);
+        return redirect()->back();
+    }
+
+    public function cancelRequest($id)
+    {
+        DB::table('pembelians')->where('id', $id)->update([
+            'status' => 'waiting',
+            'kurir_id' => NULL,
+        ]);
+        return redirect()->back();
+    }
+
+    public function declineRequest($id)
+    {
+        DB::table('pembelians')->where('id', $id)->update([
+            'status' => 'decline'
+        ]);
+        return redirect()->back();
     }
 }
