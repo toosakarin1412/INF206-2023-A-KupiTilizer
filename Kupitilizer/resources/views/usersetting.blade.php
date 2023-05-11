25a486aa821cf9f6104e2ec7fb45275951e96850
Danish <m_danish@mhs.unsyiah.ac.id>
2023-05-11 14:41:29 +0700

    feat:user setting

--- Kupitilizer/resources/views/usersetting.blade.php
+++ Kupitilizer/resources/views/usersetting.blade.php
@@ -1,6 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
-    <p class="text-center text-3xl font-bold m-10">To Be Added Soon</p>
-    <img src="{{asset('images/mt.jpeg')}}" class="w-full p-32 pt-0"></img>
+    <div class="w-full text-center my-5 font-bold text-xl">
+        <p>User Profile</p>
+    </div>
+
+    <div class="m-10 py-5 shadow-2xl border-2 border-gray rounded-xl p-5">
+    
+    </div>
 @endsection
