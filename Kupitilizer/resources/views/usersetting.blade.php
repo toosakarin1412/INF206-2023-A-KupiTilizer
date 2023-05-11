40ef61bd5a53a58b29a38821ece2a191db8f346d
Danish <m_danish@mhs.unsyiah.ac.id>
2023-05-11 15:27:06 +0700

    feat:editsetting

--- Kupitilizer/resources/views/usersetting.blade.php
+++ Kupitilizer/resources/views/usersetting.blade.php
@@ -34,7 +34,14 @@
                 <div class="px-4 py-2">-</div>
             </div>
         </div>
-        
+        <div>
+        <!-- Edit button -->
+        <form class='px-4'>                
+            <div class="flex justify-end">
+                <button type="submit" class="text-white bg-leaf font-medium rounded-lg text-sm px-5 py-2.5 text-center">Edit</button>
+            </div>
+        </form>
+        </div>
     </div>
 
 </div>
