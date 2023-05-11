ff4ed9ccd7269c8e7fc4ec121348aa6612e59063
Danish <m_danish@mhs.unsyiah.ac.id>
2023-05-11 14:41:47 +0700

    feat:setting

--- Kupitilizer/resources/views/usersetting.blade.php
+++ Kupitilizer/resources/views/usersetting.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="w-full text-center my-5 font-bold text-xl">
-        <p>User Profile</p>
+        <p>Setting</p>
     </div>
 
     <div class="m-10 py-5 shadow-2xl border-2 border-gray rounded-xl p-5">
